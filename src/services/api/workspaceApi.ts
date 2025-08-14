b2e0cb5315b9846c0cce4f35278302dc8d712aea
devzeus <hieu01bdvn@gmail.com>
2025-08-14 19:40:33 +0700

    Fix show channels when created succesffully

--- src/services/api/workspaceApi.ts
+++ src/services/api/workspaceApi.ts
@@ -17,47 +17,11 @@ export interface Participant {
   avatarUrl?: string | null;
 }
 
-export interface ChatMessageResponse {
-  id: string;
-  channelId?: string | null;
-  me: boolean;
-  message: string;
-  sender: Participant;
-  createdDate: string;
-}
-
-export interface BasicChannelResponse {
-  id: string;
-  channelName: string;
-  participantHash?: string | null;
-}
-
-export interface ChannelResponse {
-  id: string;
-  participantHash?: string | null;
-  channelName: string;
-  participants?: Participant[];
-  messages?: ChatMessageResponse[] | null;
-}
-
-export interface CreateChannelRequest {
-  workspaceId: string;
-  name: string;
-  description?: string;
-  memberIds?: string[];
-}
-
 export interface WorkspaceResponse {
   id: string;
   name: string;
   description: string;
   avatarUrl: string;
-  channels: ChannelResponse[];
-}
-
-export interface ChatMessageRequest {
-  channelId: string;
-  message: string;
 }
 
 export const getWorkspaces = async (
@@ -79,49 +43,11 @@ export const getWorkspaceById = async (
   return respoonse.data.result;
 };
 
-export const getBasicChannelByWorkspaceId = async (
-  workspaceId: string
-): Promise<BasicChannelResponse> => {
-  const response = await axiosInstance.get<ApiResponse<BasicChannelResponse>>(
-    `/server/channels/basic/${workspaceId}`
-  );
-  return response.data.result;
-};
-
-export const sendMessage = async (
-  request: ChatMessageRequest
-): Promise<ChatMessageResponse> => {
-  const response = await axiosInstance.post<ApiResponse<ChatMessageResponse>>(
-    `/server/messages/send`,
-    request
-  );
-  return response.data.result;
-};
-
-export const getChannel = async (
-  channelId: string
-): Promise<ChannelResponse> => {
-  const response = await axiosInstance.get<ApiResponse<ChannelResponse>>(
-    `/server/channels/${channelId}`
-  );
-  return response.data.result;
-};
-
 export const getStudentsByMSSV = async (
   mssv: string
 ): Promise<UserResponse[]> => {
   const response = await axiosInstance.get<ApiResponse<UserResponse[]>>(
     `/identity/users/students?mssv=${mssv}`
-  );
-  return response.data.result;
-};
-
-export const createChannel = async (
-  request: CreateChannelRequest
-): Promise<ChannelResponse> => {
-  const response = await axiosInstance.post<ApiResponse<ChannelResponse>>(
-    `/server/channels/create`,
-    request
   );
   return response.data.result;
 };
