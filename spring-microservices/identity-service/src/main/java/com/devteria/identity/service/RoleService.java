8d3694c1462c1069964c9e77b74ff1774c0c8114
devzeus <hieu01bdvn@gmail.com>
2025-08-23 11:27:14 +0700

    feat(updateUserApis): refactor user apis and role apis.

--- spring-microservices/identity-service/src/main/java/com/devteria/identity/service/RoleService.java
+++ spring-microservices/identity-service/src/main/java/com/devteria/identity/service/RoleService.java
@@ -28,9 +28,6 @@ public class RoleService {
     public RoleResponse create(RoleRequest request) {
         var role = roleMapper.toRole(request);
 
-        var permissions = permissionRepository.findAllById(request.getPermissions());
-        role.setPermissions(new HashSet<>(permissions));
-
         role = roleRepository.save(role);
         return roleMapper.toRoleResponse(role);
     }
