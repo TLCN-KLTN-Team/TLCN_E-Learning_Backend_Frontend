6789168c0c7a271d9fc2dc4897dfc9d647739da6
devzeus <hieu01bdvn@gmail.com>
2025-09-11 01:04:46 +0700

    get old source

--- react-type-vite/src/pages/system-admin/SystemAdminDashboard.tsx
+++ react-type-vite/src/pages/system-admin/SystemAdminDashboard.tsx
@@ -10,7 +10,6 @@ import AccountManagement from "../../components/system-admin/AccountManagement";
 import CategoryManagement from "../../components/system-admin/CategoryManagement";
 import RevenueManagement from "../../components/system-admin/RevenueManagement";
 import SystemStatistics from "../../components/system-admin/SystemStatistics";
-import AdminProfilePage from "../../components/system-admin/AdminProfilePage";
 import "../../styles/system-admin.css";
 
 const SystemAdminDashboard: React.FC = () => {
@@ -41,8 +40,6 @@ const SystemAdminDashboard: React.FC = () => {
         return <RevenueManagement />;
       case "statistics":
         return <SystemStatistics />;
-      case "profile":
-        return <AdminProfilePage />;
       default:
         return (
           <div className="space-y-6">
