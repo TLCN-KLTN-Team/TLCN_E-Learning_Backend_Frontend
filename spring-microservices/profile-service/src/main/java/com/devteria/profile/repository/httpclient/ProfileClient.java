08f63c4580231447210c77cdb85891985de21868
devzeus <hieu01bdvn@gmail.com>
2025-10-09 09:33:28 +0700

    (big update): update service name, and new notification service

--- spring-microservices/profile-service/src/main/java/com/devteria/profile/repository/httpclient/ProfileClient.java
+++
@@ -1,7 +0,0 @@
-package com.devteria.profile.repository.httpclient;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "get-profile", url = "http://localhost:8083")
-public interface ProfileClient {
-}
