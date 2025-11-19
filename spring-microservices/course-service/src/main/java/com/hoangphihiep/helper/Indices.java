8d22d8868e9cb699e2a738ad1e08b7887a48bc67
devzeus <hieu01bdvn@gmail.com>
2025-11-19 17:15:07 +0700

    (search features): search with elasticsearch and auto completion

--- spring-microservices/course-service/src/main/java/com/hoangphihiep/helper/Indices.java
+++ spring-microservices/course-service/src/main/java/com/hoangphihiep/helper/Indices.java
@@ -2,6 +2,7 @@ package com.hoangphihiep.helper;
 
 public final class Indices {
     public static final String PUBLISHED_COURSE_INDEX = "published_courses";
+    public static final String PUBLISHED_COURSE_INDEX_NEW = "published_courses_new";
 
     private Indices() {
         // Private constructor to prevent instantiation
