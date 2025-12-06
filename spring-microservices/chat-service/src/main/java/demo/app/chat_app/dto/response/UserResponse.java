ad79e3202f671a29c1c2a535910a8e832287cc4b
devzeus <hieu01bdvn@gmail.com>
2025-12-06 14:06:28 +0700

    (workspace refactor): update message and fix features that related to chat realtime

---
+++ spring-microservices/chat-service/src/main/java/demo/app/chat_app/dto/response/UserResponse.java
@@ -0,0 +1,16 @@
+package demo.app.chat_app.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserProfileResponse {
+    String id;
+    String firstName;
+    String lastName;
+    String avatarUrl;
+}
