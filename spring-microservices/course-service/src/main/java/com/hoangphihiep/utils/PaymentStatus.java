1f8767c9e091f7f2d2d5de57dfd9a2df5e8ea725
hoangphihiep <phihiep31012004@gmail.com>
2025-12-07 14:41:48 +0700

    fix

--- spring-microservices/course-service/src/main/java/com/hoangphihiep/utils/PaymentStatus.java
+++ spring-microservices/course-service/src/main/java/com/hoangphihiep/utils/PaymentStatus.java
@@ -1,10 +1,6 @@
 package com.hoangphihiep.utils;
 
 public enum PaymentStatus {
-<<<<<<< HEAD
-=======
-        HEAD,
->>>>>>> b032604 (revenue function, update lesson correction, update review)
         PENDING,
         PAID,
         REFUNDED
